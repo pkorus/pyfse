48fb33b15586349eacbbb8cec2191cd847668e12
Paweł Korus <pkorus@gmail.com>
2019-05-16 23:24:08 -0400

    Rudimentary cleanup and more tests

--- pyfse.pyx
+++ pyfse.pyx
@@ -1,34 +1,46 @@
+from libc.stdlib cimport malloc, free
+
 cdef extern from "fse.h":
 
     size_t FSE_compress(void* dst, size_t dstCapacity, const void* src, size_t srcSize);
 
     size_t FSE_decompress(void* dst,  size_t dstCapacity, const void* cSrc, size_t cSrcSize);
 
+    size_t FSE_compressBound(size_t size);
+
+    unsigned FSE_isError(size_t code);
+
+    const char* FSE_getErrorName(size_t code);
+
 def py_hello(name: bytes) -> None:
     print("Hello")
 
 def compress(src: bytes) -> bytes:
-    print("Compressing", src)
-    cdef unsigned char dst[2000]
+    # print("Compressing", src)
+    cdef unsigned int dst_size = FSE_compressBound(len(src))
+    # print('Recommended size', dst_size)
+    cdef unsigned char *dst = <unsigned char*> malloc(dst_size * sizeof(unsigned char))
     cdef unsigned char * src_ptr = src 
-    dst[0] = 10
-    print("Init dst", dst)
-    ret = FSE_compress(&dst, 2000, src_ptr, len(src))
-    print('Comp ret: ', ret)
-    print('Comp out: ', dst)
-    return dst
+    # print("Init dst", dst)
+    ret = FSE_compress(dst, dst_size, src_ptr, len(src))
+    # print('Comp ret: ', ret)
+    # print('Comp out: ', dst)
+    output_list = bytes(dst) # Converts
+    free(dst)
+    return output_list
 
 def decompress(src: bytes) -> bytes:
-    print("Decompressing", src)
-    cdef unsigned char dst[10]
+    # print("Decompressing", src)
+    cdef unsigned int dst_size = 10 * len(src)
+    # print('Recommended size', dst_size)
+    cdef unsigned char *dst = <unsigned char*> malloc(dst_size * sizeof(unsigned char))
     cdef unsigned char * src_ptr = src 
-    dst[0] = 12
-    dst[1] = 0
-    print("Init dst", dst)
-    ret = FSE_decompress(&dst, 10, src_ptr, len(src))
-    if ret == 0:
-        raise ValueError("Error")
-    dst[ret] = 0
-    print('Decomp ret: ', ret)
-    print('Decomp out: ', dst)
-    return dst
+    # print("Init dst", dst)
+    ret = FSE_decompress(dst, dst_size, src_ptr, len(src))
+    if FSE_isError(ret):
+        raise ValueError("Decoding Error: {}".format(FSE_getErrorName(ret)))
+    # print('Decomp ret: ', ret)
+    # print('Decomp out: ', dst)
+    output_list = bytes(dst) # Converts
+    free(dst)
+    return output_list
--- test.py
+++ test.py
@@ -1,12 +1,55 @@
+import numpy as np
+import pickle
+import deepdish
+from collections import Counter
+
 import pyfse
+import dahuffman
 
-input = b"Hello world, hello all, hello world, Hello, Hello, world, world"
-input = bytes([1, 1, 2, 3, 4, 1, 2, 3, 2, 1, 1, 1, 1, 2, 2, 2, 1, 1, 3, 2, 4])
+def shortstr(d, n=40):
+    n2 = n // 2 - 2
+    if len(d) > n:
+        return ''.join((d[:n2].decode('utf8')) + ' .. ' + (d[-n2:].decode('utf8')))
+    else:
+        return str(d)
 
-out_c = pyfse.compress(input)
-out_d = pyfse.decompress(out_c)
+def entropy(p):
+    if type(p) is not np.ndarray:
+        p = np.array(list(p))
+    return - float(np.sum(p * np.log2(p)))
 
-print('Input: {} ({}) --> {}'.format(input, len(input), list(input)))
-print('Compressor out ({}): {}'.format(len(out_c), list(out_c)))
-print('De-compressor out ({}): {}'.format(len(out_d), list(out_d)))
-print('De-out: {}'.format(out_d))
+input = b"Hello world, hello all, hello world, Hello, Hello, world, world" * 10
+# input = bytes([1, 1, 2, 3, 4, 1, 2, 3, 2, 1, 1, 1, 1, 2, 2, 2, 1, 1, 3, 2, 4])
+n = len(input)
+n_show = 10
+
+print('Input [{}]: {}'.format(len(input), shortstr(input)))
+
+# Gather stats
+input_stats = dict(Counter(input))
+input_prob = {k: v/n for k, v in input_stats.items()}
+input_entropy = entropy(input_prob.values())
+
+print('Frequency stats:')
+print('  Symbol probabilities: {}'.format({k: round(v, 2) for k, v in input_prob.items()}))
+print('  Entropy: {:.2f}'.format(input_entropy))
+
+# Python implementation of the Huffman codec
+codec = dahuffman.HuffmanCodec.from_data(input_stats)
+codec.print_code_table()
+coded_huff = codec.encode(input)
+
+print('Table: {}'.format(codec.get_code_table()))
+
+print('Expected length: {:.1f} bytes'.format(len(input) * input_entropy / 8))
+print('Huffman coded: {} bytes ({} including p. tables)'.format(len(coded_huff),
+    len(pickle.dumps(codec.get_code_table())) + len(coded_huff)
+))
+
+# The ANS codec
+coded_fse = pyfse.compress(input)
+decoded_fse = pyfse.decompress(coded_fse)
+
+print('FSE success: {}'.format(input == decoded_fse))
+print('FSE coded: {} bytes (includes codec p. tables)'.format(len(coded_fse)))
+print('FSE decoded [{}]: {}'.format(len(decoded_fse), shortstr(decoded_fse)))
